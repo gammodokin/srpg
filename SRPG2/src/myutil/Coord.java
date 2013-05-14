bb39edc1ed122a174c4f622c2cf5473d6c07c88b
gammodokin <yf-22_raptor@hotmail.co.jp>
2013-05-15 00:27:04 +0900

    座標の比較方法を修正

--- SRPG2/src/myutil/Coord.java
+++ SRPG2/src/myutil/Coord.java
@@ -4,33 +4,33 @@ package myutil;
 public class Coord<E extends Number> implements Cloneable {
 
 	public E x, y;
-	
+
 	public Coord(E x ,E y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public void assign(Coord<E> co) {
 		x = co.x;
 		y = co.y;
 	}
-	
+
 	@Override
 	public Coord<E> clone() {
 		return new Coord<E>(x, y);
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
 		Coord<E> co = (Coord<E>)o;
-		return co.x == x && co.y == y;
+		return co.x.equals(x) && co.y.equals(y);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return (int)(x.doubleValue() * 0xffff + y.doubleValue());
 	}
-	
+
 	@Override
 	public String toString() {
 		return "(" + x + ", " + y + ")";
